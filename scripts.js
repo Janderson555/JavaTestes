7a3077affd8e8b3d42a21e6dc1892b78151ace8d
Janderson Soares <jandinsm@gmail.com>
2021-04-25 21:15:15 -0300

    Primeiros passos Java.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let Janderson = 60;
+
+if (Janderson >= 80){
+    console.log("Parabens!!");
+} else if(Janderson < 80 && Janderson >= 60){
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovado.");
+}
